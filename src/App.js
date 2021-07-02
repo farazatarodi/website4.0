2f5ee676004b732e2d34fc74384ffdfddc2bdc62
farazatarodi <farazatarodi@gmail.com>
2021-07-02 02:08:59 +0200

    added doggo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import './App.css';
 import scroll from './down-arrow.png';
 
+const doggoCome = (e) => {
+  const doggo = document.querySelector('.doggo');
+  if (e.clientX < (window.innerWidth - doggo.offsetWidth) * 0.98) {
+    doggo.style.left = e.clientX + 'px';
+  }
+
+  doggo.style.animation = 'bg 0.5s steps(1) infinite alternate';
+
+  if (doggo.offsetLeft < e.clientX) {
+    doggo.style.transform = 'translate(-50%) scaleX(-1)';
+  } else {
+    doggo.style.transform = 'translate(-50%) scaleX(1)';
+  }
+  if (
+    doggo.offsetLeft <= e.clientX * 1.05 &&
+    doggo.offsetLeft >= e.clientX * 0.95
+  ) {
+    console.log('yes');
+    doggo.style.animation = '';
+    doggo.style.backgroundImage = "url('0.png')";
+  }
+};
+
+const doggoSit = (e) => {
+  const doggo = document.querySelector('.doggo');
+  console.log(doggo.offsetLeft, e.clientX);
+
+  if (
+    doggo.offsetLeft <= e.clientX * 1.05 &&
+    doggo.offsetLeft >= e.clientX * 0.95
+  ) {
+    console.log('yes');
+    doggo.style.animation = 'bg 0.5s steps(1)';
+    doggo.style.backgroundImage = "url('0.png')";
+  }
+};
+
 function App() {
   return (
     <div className="App">
       <div className="scroll-container">
-        <section className="one" id="home">
+        <section
+          className="one"
+          id="home"
+          onMouseMove={doggoCome}
+          onMouseOver={doggoSit}
+        >
           <div className="header">
             <div className="sec1">
               <a href="#home">HOME</a>
@@ -22,6 +64,9 @@ function App() {
             {'{'}
             <span style={{ color: '#00ffcc' }}>developer</span>
             {'}'}
+          </div>
+          <div className="doggo-div">
+            <div className="doggo"></div>
           </div>
           <img src={scroll} className="scroll-animation" alt=""></img>
         </section>
