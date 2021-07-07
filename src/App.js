3b04cb9b6544e005c04c60dcd33bef35c6d2a16d
farazatarodi <farazatarodi@gmail.com>
2021-07-07 16:58:25 +0200

    Added projects

--- src/App.js
+++ src/App.js
@@ -8,7 +8,25 @@ import Doggo from './Doggo';
 import Skill from './Skill';
 import Edu from './Edu';
 import python from './media/logos/python.png';
-import Project from './Project.js';
+import Project from './Project';
+
+// image import
+import two from './media/bg/2.0.png';
+import four from './media/bg/4.0.png';
+import av from './media/bg/av.png';
+import bb from './media/bg/bb.png';
+import board from './media/bg/board.png';
+import disk from './media/bg/disk.png';
+import ha from './media/bg/ha.png';
+import iot from './media/bg/iot.png';
+import iran from './media/bg/iran.png';
+import mc from './media/bg/mc.png';
+import os from './media/bg/os.png';
+import qr from './media/bg/qr.png';
+import robot from './media/bg/robot.png';
+import siemens from './media/bg/siemens.png';
+import task from './media/bg/task.png';
+import vision from './media/bg/vision.png';
 
 // scroll direction function
 var oldLeft = 0;
@@ -78,6 +96,135 @@ const scrollDirection = () => {
     }
   }, 1200);
 };
+
+// project data
+const projectData = [
+  {
+    id: 'siemens',
+    color: '#FF5420',
+    title: 'Siemens Healthineers',
+    bg: siemens,
+    desc: 'One year of full-time job at Siemens Healthineers as an ultrasound engineer',
+  },
+  {
+    id: 'os',
+    color: '#F48636',
+    title: 'Omran Soule Co.',
+    bg: os,
+    desc: 'Part-time job at Omran Soule Co. as a python developer and full stack engineer',
+  },
+  {
+    id: 'robot',
+    color: '#F7EBE1',
+    title: 'Robot Path Simulator',
+    bg: robot,
+    desc: 'A small script in Python that turns strings into movement commands for a robot',
+  },
+  ,
+  {
+    id: 'iran',
+    color: '#F7C69E',
+    title: 'Iran Population Map',
+    bg: iran,
+    desc: 'An interactive population map of Iran using React, Python, Deck.gl and Mapbox',
+  },
+  ,
+  {
+    id: 'iot',
+    color: '#FECB4B',
+    title: 'IoT Boards',
+    bg: iot,
+    desc: 'A study on the possibility of imlplementing SBCs into home appliences',
+  },
+  ,
+  {
+    id: 'bb',
+    color: '#3D6033',
+    title: 'Breaking Bad Characters',
+    bg: bb,
+    desc: 'Another small script in Python that outputs episode numbers of Breaking Bad series based on provided charachter input',
+  },
+  ,
+  {
+    id: 'mc',
+    color: '#7CBD6B',
+    title: 'Minecraft Farm Wiki',
+    bg: mc,
+    desc: 'A hobby website that deals with technical aspect of Minecraft',
+  },
+  ,
+  {
+    id: 'vision',
+    color: '#2F9A80',
+    title: 'Computer Vision and Smart Assistant',
+    bg: vision,
+    desc: 'A system that gets inputs from the camera and adapts the assistant based on the situation',
+  },
+  ,
+  {
+    id: 'four',
+    color: '#00FFCC',
+    title: 'Personal Website v4.0',
+    bg: four,
+    desc: 'This website! Created with React',
+  },
+  ,
+  {
+    id: 'qr',
+    color: '#30D9D9',
+    title: 'Object and QR code Detection',
+    bg: qr,
+    desc: 'A Python project to navigate a drone based on the camera input and scan QR codes to gather points',
+  },
+  ,
+  {
+    id: 'task',
+    color: '#011936',
+    title: 'Task Tracker',
+    bg: task,
+    desc: 'A simple task tracker in React',
+  },
+  ,
+  {
+    id: 'two',
+    color: '#323840',
+    title: 'Personal Website v2.0',
+    bg: two,
+    desc: "Previous version of my personal website. It's in Vanilla JS",
+  },
+  ,
+  {
+    id: 'disk',
+    color: '#A3ACBF',
+    title: 'Disk Sorter',
+    bg: disk,
+    desc: 'A disk sorter based on varying thicknesses of disks.',
+  },
+  ,
+  {
+    id: 'av',
+    color: '#252140',
+    title: 'Autonomous Vehicle Components',
+    bg: av,
+    desc: "My bachelor's thesis. A study on different methods and components of autopilot systems with the goal of implementing said systems into ordinary vehicles",
+  },
+  ,
+  {
+    id: 'ha',
+    color: '#F7C8D6',
+    title: 'Hamrahan Atebba Co.',
+    bg: ha,
+    desc: 'Part-time job at Hamrahan Atebba Co. as an IT manager and full stack engineer',
+  },
+  ,
+  {
+    id: 'board',
+    color: '#B3444F',
+    title: 'Self-balancing robot',
+    bg: board,
+    desc: 'A self-balancing transportation robot',
+  },
+];
 
 // Main
 function App() {
@@ -177,22 +324,15 @@ function App() {
         </section>
         <section className="three" id="projects">
           <div className="project-wrapper">
-            <Project id="iran" />
-            <Project id="4" />
-            <Project id="bb" />
-            <Project id="robot" />
-            <Project id="2" />
-            <Project id="task" />
-            <Project id="mc" />
-            <Project id="siemens" />
-            <Project id="os" />
-            <Project id="ha" />
-            <Project id="disk" />
-            <Project id="av" />
-            <Project id="vision" />
-            <Project id="qr" />
-            <Project id="iot" />
-            <Project id="balancing" />
+            {projectData.map(({ id, color, title, bg, desc }) => (
+              <Project
+                id={id}
+                color={color}
+                title={title}
+                bg={bg}
+                desc={desc}
+              />
+            ))}
           </div>
         </section>
         <Doggo />
--- src/Project.js
+++ src/Project.js
@@ -1,5 +1,51 @@
-const Project = () => {
-  return <div className="project"></div>;
+const Project = ({ id, color, title, bg, desc }) => {
+  const expand = () => {
+    const project = document.querySelector('#' + id);
+    const title = document.querySelector('#' + id + '-title');
+    const desc = document.querySelector('#' + id + '-desc');
+    project.style.width = '15%';
+    project.style.backgroundImage =
+      'linear-gradient(135deg,' +
+      color +
+      ' 0% ,' +
+      color +
+      ' 35%, rgba(0,0,0,0) 100%), url("' +
+      bg +
+      '")';
+    title.style.transform = 'rotate(0deg)';
+    desc.style.animation = 'show 0.2s steps(1)';
+    desc.style.display = 'initial';
+    desc.style.visibility = 'visible';
+  };
+
+  const shrink = () => {
+    const project = document.querySelector('#' + id);
+    const title = document.querySelector('#' + id + '-title');
+    const desc = document.querySelector('#' + id + '-desc');
+    project.style.width = '4%';
+    project.style.backgroundImage = '';
+    title.style.transform = 'rotate(-90deg)';
+    desc.style.animation = '';
+    desc.style.display = 'none';
+    desc.style.visibility = 'hidden';
+  };
+
+  return (
+    <div
+      id={id}
+      className="project"
+      style={{ backgroundColor: color }}
+      onMouseOver={expand}
+      onMouseLeave={shrink}
+    >
+      <div id={id + '-title'} className="project-title">
+        {title}
+      </div>
+      <div id={id + '-desc'} className="project-description">
+        {desc}
+      </div>
+    </div>
+  );
 };
 
 export default Project;
