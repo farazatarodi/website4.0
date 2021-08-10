73682ffc2ae48f4329415410753554377365f133
farazatarodi <farazatarodi@gmail.com>
2021-08-10 18:41:39 +0200

    added summary to all project pages

--- src/Projects/OS.js
+++ src/Projects/OS.js
@@ -1,7 +1,5 @@
 import Nav from './Nav';
 import bg from '../media/title/os2.png';
-import paper from '../media/Faraz_Atarodi-Bachelor_Thesis.pdf';
-import linkArrow from '../media/diagonal-arrow.png';
 
 const OS = () => {
   return (
@@ -11,64 +9,23 @@ const OS = () => {
         <div className="pr-row1">
           <div className="pr-col1">
             <div className="pr-title">Omran Soule Co.</div>
-            <div className="pr-desc">2017 - 2018</div>
+            <div className="pr-desc">2016 - 2017</div>
             <div className="pr-desc-title">Type:</div>
-            <div className="pr-desc">Bachelor's Thesis</div>
+            <div className="pr-desc">Part-time Job</div>
             <div className="pr-desc-title">Role:</div>
-            <div className="pr-desc">Student</div>
+            <div className="pr-desc">
+              Full-stack Engineer / Python Developer
+            </div>
             <div className="pr-desc-title">Technologies:</div>
             <div className="pr-desc" style={{ marginBottom: '1rem' }}>
-              Python - Breaking Bad API - PyTest
-            </div>
-            <div className="pr-link">
-              <a
-                className="links"
-                href={paper}
-                target="_blank"
-                rel="noreferrer"
-              >
-                View Paper
-                <img src={linkArrow} alt="" className="link-arrow" />
-              </a>
+              Python - JavaScript - CSS - HTML - VBA - SQL
             </div>
           </div>
           <div className="pr-col2">
             <img src={bg} alt="" className="pr-title-image" />
           </div>
         </div>
-        <div className="pr-row2">
-          I studied mechatronics engineering for my bachelor's degree in my
-          hometown of Tehran, Iran. My program consisted of three main subjects,
-          mechanical and electronics engineering and computer science. For my
-          final thesis, I decided to study a topic that covers all of these
-          subjects. The autonomous vehicles topic was the first thing that came
-          to mind as it is a rather traditional industry going through
-          modernization. During my initial research, I realized that there are
-          extensive papers regarding the technology surrounding the topic.
-          However, one approach was not studied and that was the approach of
-          converting current vehicles to autonomous vehicles.
-          <br />I managed to get one of the most published professors in my
-          country as a supervisor and with his approval, I began my main
-          research. After several months, I provided a detailed report with
-          great accuracy on the components needed for this conversion. This
-          initial report although detailed did not satisfy my supervisor. He
-          needed me to come up with a method to make this conversion process
-          optimized.
-          <br />
-          The next step for me was to study each component and understand its
-          functionality. This process helped me understand the relationship
-          between components and how I can add or remove elements to make the
-          system more efficient.
-          <br />
-          One of the main subjects in my paper was the implementation of speed
-          controllers and proximity sensors to achieve complete speed control. I
-          studied and provided several controllers and different technologies
-          that can be used for this purpose.
-          <br />
-          This paper is in my native language of Persian, but I have provided a
-          download link just in case. I hope you enjoy reading it and find it
-          useful.
-        </div>
+        <div className="pr-row2"></div>
       </div>
     </div>
   );
--- src/Projects/Two.js
+++ src/Projects/Two.js
@@ -1,6 +1,5 @@
 import Nav from './Nav';
 import bg from '../media/title/2.0.png';
-import paper from '../media/Faraz_Atarodi-Bachelor_Thesis.pdf';
 import linkArrow from '../media/diagonal-arrow.png';
 
 const Two = () => {
@@ -11,23 +10,23 @@ const Two = () => {
         <div className="pr-row1">
           <div className="pr-col1">
             <div className="pr-title">Personal Website V2.0</div>
-            <div className="pr-desc">2017 - 2018</div>
+            <div className="pr-desc">February 2021</div>
             <div className="pr-desc-title">Type:</div>
-            <div className="pr-desc">Bachelor's Thesis</div>
+            <div className="pr-desc">Hobby</div>
             <div className="pr-desc-title">Role:</div>
-            <div className="pr-desc">Student</div>
+            <div className="pr-desc">Web Designer</div>
             <div className="pr-desc-title">Technologies:</div>
             <div className="pr-desc" style={{ marginBottom: '1rem' }}>
-              Python - Breaking Bad API - PyTest
+              JavaScript - CSS - HTML
             </div>
             <div className="pr-link">
               <a
                 className="links"
-                href={paper}
+                href="https://farazatarodi.github.io/website2.0/"
                 target="_blank"
                 rel="noreferrer"
               >
-                View Paper
+                View Website
                 <img src={linkArrow} alt="" className="link-arrow" />
               </a>
             </div>
@@ -36,39 +35,7 @@ const Two = () => {
             <img src={bg} alt="" className="pr-title-image" />
           </div>
         </div>
-        <div className="pr-row2">
-          I studied mechatronics engineering for my bachelor's degree in my
-          hometown of Tehran, Iran. My program consisted of three main subjects,
-          mechanical and electronics engineering and computer science. For my
-          final thesis, I decided to study a topic that covers all of these
-          subjects. The autonomous vehicles topic was the first thing that came
-          to mind as it is a rather traditional industry going through
-          modernization. During my initial research, I realized that there are
-          extensive papers regarding the technology surrounding the topic.
-          However, one approach was not studied and that was the approach of
-          converting current vehicles to autonomous vehicles.
-          <br />I managed to get one of the most published professors in my
-          country as a supervisor and with his approval, I began my main
-          research. After several months, I provided a detailed report with
-          great accuracy on the components needed for this conversion. This
-          initial report although detailed did not satisfy my supervisor. He
-          needed me to come up with a method to make this conversion process
-          optimized.
-          <br />
-          The next step for me was to study each component and understand its
-          functionality. This process helped me understand the relationship
-          between components and how I can add or remove elements to make the
-          system more efficient.
-          <br />
-          One of the main subjects in my paper was the implementation of speed
-          controllers and proximity sensors to achieve complete speed control. I
-          studied and provided several controllers and different technologies
-          that can be used for this purpose.
-          <br />
-          This paper is in my native language of Persian, but I have provided a
-          download link just in case. I hope you enjoy reading it and find it
-          useful.
-        </div>
+        <div className="pr-row2"></div>
       </div>
     </div>
   );
