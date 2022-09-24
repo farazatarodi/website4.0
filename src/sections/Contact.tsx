8ab9d11a23f4f5521914572ff3f0925cbd28189e
Faraz Atarodi <farazatarodi@gmail.com>
2022-09-24 17:35:40 +0200

    🚚 move ContactForm to components

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 
-import ContactForm from '../ContactForm';
+import ContactForm from '../components/ContactForm';
 import cv from '../media/Faraz Atarodi - CV.pdf';
 import linkArrow from '../media/diagonal-arrow.png';
 import '../css/Contact.css';
